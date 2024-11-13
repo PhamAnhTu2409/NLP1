0d9a8755099a50567e0785079f08070fa825b913
PhamAnhTu2409 <108467888+PhamAnhTu2409@users.noreply.github.com>
2024-11-14 05:01:42 +0700

    Update Lab2.py

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -1,13 +1,16 @@
 import gensim
 import re
 
+# Пара слов: досуг, прогулка
+
 # Использование регулярных выражений для извлечения слов в формате "_NOUN"
 pat = re.compile("(.*)_NOUN")
 # Определение списка позитивных слов
-pos = ["досуг_NOUN", "прогулка_PROPN"]
+pos = ["моцион_NOUN", "развлечение_NOUN"]
 neg = []  # Список негативных слов пустой
 
 word2vec = gensim.models.KeyedVectors.load_word2vec_format("cbow.txt", binary=False)
+
 
 # Получение 10 наиболее похожих слов на линейную комбинацию
 dist = word2vec.most_similar(positive=pos, topn=10)
